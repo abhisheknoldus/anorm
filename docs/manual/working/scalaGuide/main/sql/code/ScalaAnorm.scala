73f27642b83a56a1f4df182f1129a4046ba7cf98
abhisheknoldus <abhishek@knoldus.com>
2016-10-13 22:13:19 +0530

    code sample changes with db injection

--- docs/manual/working/scalaGuide/main/sql/code/ScalaAnorm.scala
+++ docs/manual/working/scalaGuide/main/sql/code/ScalaAnorm.scala
@@ -1,27 +1,14 @@
 package scalaGuide.main.sql
 
-import org.specs2.mutable.Specification
-import play.api.test._
-import play.api.test.Helpers._
+import play.api.db.Database
+import anorm._
+import com.google.inject.Inject
+//#playdb
+class ScalaAnorm  @Inject () (db: Database) {
 
-object ScalaAnorm extends Specification {
-  "Code samples" title
+  db.withConnection { implicit connection =>
+    val result: Boolean = SQL("Select 1").execute()
 
-  "Anorm" should {
-    "be usable in play" in new WithApplication(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
-      //#playdb
-      import play.api.db.Database
-      import anorm._
-      import com.google.inject.Inject
-
-
-      class PlayAnorm @Inject () (db: Database)  {
-        db.withConnection { implicit connection =>
-          val result: Boolean = SQL("Select 1").execute()
-        }
-      }
-      //#playdb
-      ok
-    }
   }
 }
+//#playdb
---
+++ docs/manual/working/scalaGuide/main/sql/code/ScalaAnormTest.scala
@@ -0,0 +1,15 @@
+/**
+  * Created by knoldus on 13/10/16.
+  */
+import org.specs2.mutable.Specification
+import play.api.test._
+import play.api.test.Helpers._
+
+object ScalaAnormTest extends Specification {
+  "Code samples" title
+
+  "Anorm" should {
+    "be usable in play" in new WithApplication() {
+
+    }
+}
